6eb9b58e246cd961ce3be770ed24b0129c970ad1
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-02-09 04:11:24 -0300

    resuelto ejercicio 1

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Lautaro
+apellido: Llusa
 ---
 Ejercicio: entrada_salida_01
 ---
@@ -26,7 +26,7 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
-        pass
+        alert("Hola mundo", "Esto no anda, funciona")
 
 
 if __name__ == "__main__":
