5b3c14f497228d8c48a4bd9841d85b44ed6f9ec2
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-02-10 06:40:21 -0300

    resuelto ejercicio 2

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Lautaro
+apellido: Llusa
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -27,6 +27,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
+        nombre_alumno = prompt("Datos", "Ingrese su nombre")
+        alert("Datos", nombre_alumno)
         pass
         
         
