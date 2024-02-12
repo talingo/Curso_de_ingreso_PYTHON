937c1c075460f550170099b8a1fe835ab41bcb71
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-02-12 07:00:55 -0300

    resuelto ejercicio 10

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_10.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_10.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Lautaro
+apellido: Llusa
 ---
 Ejercicio: entrada_salida_10
 ---
@@ -34,7 +34,13 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, padx=30, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
-        pass
+        importe = self.txt_importe.get()
+        importe = float(importe)
+        descuento = importe * 0.20
+        importe_con_descuento = importe - descuento
+        mensaje = f"El importe con un descuento del 20% es: {importe_con_descuento}"
+        alert("Resultado", mensaje)
+       
 
 
 if __name__ == "__main__":
