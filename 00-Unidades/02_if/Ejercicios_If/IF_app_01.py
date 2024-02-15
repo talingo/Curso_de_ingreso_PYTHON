66ac9859392d1a5ba93070ec062c8aa51fc9f978
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-02-14 22:27:10 -0300

    resuelto ejercicio if 01

--- 00-Unidades/02_if/Ejercicios_If/IF_app_01.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_01.py
@@ -33,7 +33,11 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        edad = self.txt_edad.get()
+        edad_int = int(edad)
+        
+        if edad_int == 18:
+            alert("UTN", "Tiene 18")
 
          
 if __name__ == "__main__":
