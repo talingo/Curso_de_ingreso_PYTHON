bfb45639f3be972cb0f183bf7a12591165d9ed6c
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-02-18 16:36:24 -0300

    resuelto ejercicio if 09

--- 00-Unidades/02_if/Ejercicios_If/IF_app_09.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_09.py
@@ -6,8 +6,8 @@ import customtkinter
 import random
 
 '''
-nombre:
-apellido:
+nombre: Lautaro
+apellido: Llusa
 ---
 Ejercicio: if_09
 ---
@@ -27,7 +27,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        numero_aleatorio = random.randint(1,10)
+
+        alert("UTN", numero_aleatorio)
         
 
 if __name__ == "__main__":
