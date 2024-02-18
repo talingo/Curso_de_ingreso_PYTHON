1199c04e555d9a2d8919856f2650509cfd004a74
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-02-18 16:32:56 -0300

    resuelto ejercicio if 08

--- 00-Unidades/02_if/Ejercicios_If/IF_app_08.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_08.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Lautaro
+apellido: Llusa
 ---
 Ejercicio: if_08
 ---
@@ -40,7 +40,13 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        edad = self.txt_edad.get()
+        edad_int = int(edad)
+
+        estado_civil = self.combobox_estado_civil.get()
+
+        if estado_civil == "Soltero" and edad_int >= 18:
+            alert("UTN", "Es soltero y no es menor")
 
         
         
