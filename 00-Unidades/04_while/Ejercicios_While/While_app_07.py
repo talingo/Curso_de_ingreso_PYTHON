7d032eb72c3b2c81b24568e06824f4155caa3ed2
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-02-25 14:15:42 -0300

    resuelto ejercicio while 7

--- 00-Unidades/04_while/Ejercicios_While/While_app_07.py
+++ 00-Unidades/04_while/Ejercicios_While/While_app_07.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Lautaro
+apellido: Llusa
 ---
 Ejercicio: while_07
 ---
@@ -36,7 +36,27 @@ class App(customtkinter.CTk):
 
 
     def btn_comenzar_ingreso_on_click(self):
-        pass
+        contador_iteracion = 0
+        acumulador_numeros = 0
+        self.txt_suma_acumulada.delete(0,"end")
+        self.txt_promedio.delete(0,"end")
+
+        while True:
+            numero = prompt("UTN","Ingrese un numero")
+
+            if numero == None:
+                break
+
+            numero = int(numero)
+            acumulador_numeros = acumulador_numeros + numero
+
+            contador_iteracion += 1
+            
+        promedio = acumulador_numeros / contador_iteracion
+
+        self.txt_suma_acumulada.insert(0, acumulador_numeros)
+        self.txt_promedio.insert(0, promedio)
+
 
     
 if __name__ == "__main__":
